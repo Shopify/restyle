f12a5c2bc80b25a87ebf526df70d5a73883d8bd2
Matt Frances <43865546+mattfrances@users.noreply.github.com>
2023-01-27 07:23:39 -0500

    Add fixture app (#212)
    * Add initial fixture app and config
    
    * Fix lint errors
    
    * Add fixture app contents
    
    * Limit fixture lint to ts and tsx files
    
    * Fix to run on Android emulator
    
    * Code cleanup

---
+++ fixture/App.tsx
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+import {
+  ThemeProvider,
+  createBox,
+  createText,
+  createRestyleComponent,
+  createVariant,
+  VariantProps,
+} from '@shopify/restyle';
+import {SafeAreaView, Switch} from 'react-native';
+
+import {theme, darkTheme, Theme} from './theme';
+
+const Box = createBox<Theme>();
+const Text = createText<Theme>();
+
+const Card = createRestyleComponent<
+  VariantProps<Theme, 'cardVariants'> & React.ComponentProps<typeof Box>,
+  Theme
+>([createVariant({themeKey: 'cardVariants'})], Box);
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const selectedTheme = darkMode ? darkTheme : theme;
+
+  return (
+    <ThemeProvider theme={selectedTheme}>
+      <Box backgroundColor="background" flex={1}>
+        <SafeAreaView style={{flex: 1}}>
+          <Box flex={1} paddingHorizontal="m" gap="s">
+            <Text variant="header">Welcome</Text>
+            <Card variant="primary">
+              <Text variant="body">
+                This is a simple example displaying how to use Restyle
+              </Text>
+            </Card>
+            <Card variant="secondary">
+              <Text variant="body">
+                You can find the theme in theme.ts. Update the theme values to
+                see how it changes this screen
+              </Text>
+            </Card>
+            <Card
+              variant="primary"
+              flexDirection="row"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Text variant="body">Toggle dark mode</Text>
+              <Switch
+                value={darkMode}
+                onValueChange={(value: boolean) => setDarkMode(value)}
+              />
+            </Card>
+          </Box>
+        </SafeAreaView>
+      </Box>
+    </ThemeProvider>
+  );
+};
+
+export default App;
