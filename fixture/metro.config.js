83ef49ce6ad74e3bd2d27bda23cc20d52d2d2159
Miguel Ninno Daipré <migueldaipre@gmail.com>
2023-10-09 13:37:30 -0300

    feat: extends metro config from @react-native/metro-config

--- fixture/metro.config.js
+++ fixture/metro.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 
@@ -17,34 +18,39 @@ const watchFolders = [
   path.join(__dirname, '../node_modules'),
 ];
 
-module.exports = (() => {
-  const config = {
-    projectRoot: __dirname,
-    transformer: {
-      getTransformOptions: () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-    },
-    resolver: {
-      blockList: exclusionList(
-        modules.map(
-          m =>
-            new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
-        ),
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  projectRoot: __dirname,
+  transformer: {
+    getTransformOptions: () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    blockList: exclusionList(
+      modules.map(
+        m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
       ),
+    ),
 
-      extraNodeModules: modules.reduce((acc, name) => {
-        acc[name] = path.join(__dirname, 'node_modules', name);
-        return acc;
-      }, {}),
-    },
-    watchFolders,
-  };
-  if (process.env.CI === 'true') {
-    config.maxWorkers = 1;
-  }
-  return config;
-})();
+    extraNodeModules: modules.reduce((acc, name) => {
+      acc[name] = path.join(__dirname, 'node_modules', name);
+      return acc;
+    }, {}),
+  },
+  watchFolders,
+};
+
+if (process.env.CI === 'true') {
+  config.maxWorkers = 1;
+}
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
